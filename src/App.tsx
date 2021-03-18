a0ae9991e1871e46b59463bc6be6961cab7a12f7
Volodymyr Tsurko <chegnenado@yahoo.com>
2021-03-18 21:13:05 +0200

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { State } from './Interfaces/State';
+
+const App: React.FC = () => {
+  const [state, setState] = useState<State>({
+    userName: 'John',
+    imageUrl: 'https://picsum.photos/600/150'
+  });
+
+  const myStyles = {margin: 40};
+
+  return (
+    <div style={myStyles}>
+      <h1>{state.userName}</h1>
+      <img src={state.imageUrl} alt="" />
+    </div>
+  );
+}
+
+export default App;
